d9b177f2c52a7cf6c9eb91769ba31e9989a1763f
ibtihel23001 <168473142+ibtihel23001@users.noreply.github.com>
2026-06-13 11:35:41 +0100

    Update api-config.schema.ts

--- mapy-backend/src/modules/api-config/api-config.schema.ts
+++ mapy-backend/src/modules/api-config/api-config.schema.ts
@@ -6,6 +6,6 @@ export const upsertApiConfigSchema = z.object({
   imapHost: z.string().default('imap.gmail.com'),
   imapPort: z.coerce.number().default(993),
   isActive: z.boolean().default(true),
-});
+});;;
 
 export type UpsertApiConfigInput = z.infer<typeof upsertApiConfigSchema>;
